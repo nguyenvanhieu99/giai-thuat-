917dcd507548a6d42eafd9f6341dcde13454a098
VanHieu <nvhieu.dev@gmail.com>
2020-10-01 22:00:41 +0700

    add tinh tong ta 1

---
+++ Thuat toan ung dung nang cao samsung/daycontongk.cpp
@@ -0,0 +1,13 @@
+//chia de chi or cua so truot
+//chia de tri
+//s[t]=a[0]+a[1]+... +a[t];
+//s[t]=s[t]+k;if(s[j]==s[k]) 
+#inlcude<iostream>
+
+using namespace std;
+
+int main(){
+	
+
+	return 0;
+}
